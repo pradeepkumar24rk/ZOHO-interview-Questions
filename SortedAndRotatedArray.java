1a914b2cf587221f8fe16b8ad53a203e2677a5c0
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2023-12-16 14:21:41 +0530

    Explaination is added

--- SortedAndRotatedArray.java
+++ SortedAndRotatedArray.java
@@ -27,3 +27,24 @@ public class SortedAndRotatedArray {
         System.out.println(findingValue(arr, target));
     }
 }
+
+//Input
+// arr: 23, 34, 45, 66, 1, 12
+// target: 1
+
+// Output
+// Founded at the index of: 4
+
+// Explaination:
+//     1
+// 66      12
+// 45      23
+//     34
+// find the mid element of the array then check the mid with the start index value
+// arr[i] <= arr[mid]
+//     whether the mid value is greater than or equal to mid then, that part will be 
+// [23,34,45,66] are one part of sorted element 
+// or
+// if arr is [23,34,45,1,2,12] then [1,2,12] is other part of sorted element
+// then check the target value is under the sorted part of other 
+// according to that change the starting and ending position.
