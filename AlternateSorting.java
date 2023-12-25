bf3fb9634d6b3819db9425f3a539731846288c83
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2023-12-25 19:34:40 +0530

    zoho round 2#14 is added

---
+++ AlternateSorting.java
@@ -0,0 +1,10 @@
+// 14.
+public class AlternateSorting {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
+}
