5f2635aee6c2306b788ab85d90d0329600ec598c
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-01-04 22:53:38 +0530

    chessQuessMoves is completed

--- ChessQueenMoves.java
+++ ChessQueenMoves.java
@@ -6,23 +6,35 @@ public class ChessQueenMoves {
 
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
-        // int row = 5,col = 5;
+        int row = 5, col = 5;
         int sourceRow = 2, sourceCol = 1;
-        int board[][] = {
-            {0,0,0,0,0},
-            {0,0,0,0,0},
-            {0,1,0,0,0},
-            {0,0,0,0,0},
-            {0,0,0,0,0},
-        };
-        int drow = 1,dcol = 0;
-        if(drow == sourceRow || sourceCol == dcol ) {
-            System.out.println("valid");
-        } 
-        else if(drow+dcol == sourceRow+sourceCol || drow-dcol == sourceRow-sourceCol){
-            System.out.println("valid");
+        int board[][] = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == sourceRow && j == sourceCol)
+                    board[i][j] = 1;
+                else
+                    board[i][j] = 0;
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
         }
-        else 
+        System.out.println();
+        int drow = 1, dcol = 0;
+        if (drow == sourceRow || sourceCol == dcol || drow + dcol == sourceRow + sourceCol
+                || drow - dcol == sourceRow - sourceCol) {
+            System.out.println("valid");
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (i == drow && j == dcol)
+                        board[i][j] = 1;
+                    else
+                        board[i][j] = 0;
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } else
             System.out.println("not valid");
     }
 }
